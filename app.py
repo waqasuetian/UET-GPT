8ddbd3718b352621a70469c51cf5609a558b31cf
waqasuetian <97124934+waqasuetian@users.noreply.github.com>
2024-09-02 19:47:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 ## RAG Q&A Conversation With PDF Including Chat History
 import streamlit as st
-#from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-#from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
