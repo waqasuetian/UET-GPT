ad83c0038a4b3e434075b59b606951c9ba92c544
waqasuetian <97124934+waqasuetian@users.noreply.github.com>
2024-09-05 16:56:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
-os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+#os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
 
-#HF_TOKEN = st.secrets["HF_TOKEN"]
+HF_TOKEN = st.secrets["HF_TOKEN"]
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
